ae3c1527248f63f3723d7dc091ca46abbc335ab4
nicholasm185 <43334128+nicholasm185@users.noreply.github.com>
2020-12-15 14:40:14 +0700

    awefawe

--- index.js
+++ index.js
@@ -196,6 +196,7 @@ function mainGameLoop(players, sockets){
     // play card event
     p1S.on('playCard', function(e){
         console.log('p1 tries to play ' + e.cardNo);
+        console.log(typeof e.cardNo);
         if(turn == 1){
             if(p1.hand.includes(e.cardNo)){
                 console.log('playing card: ' + e.cardNo);
@@ -223,6 +224,7 @@ function mainGameLoop(players, sockets){
 
     p2S.on('playCard', function(e){
         console.log('p2 tries to play ' + e.cardNo);
+        console.log(typeof e.cardNo);
         if(turn == 2){
             if(p2.hand.includes(e.cardNo)){
                 console.log('playing card: ' + e.cardNo);
